0c0d546da0bff415ee7f24a1144f03f682220d68
Mcvean Soans <mc.soans02@gmail.com>
2021-04-14 15:55:29 +0530

    add: projects page

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,20 +5,20 @@ const FooterComponent = () => {
     return(
         <>
             <footer>
-                <Container className="mt-4">
+                <Container className="my-4">
                     <Row className="">
                         <Col md={{ span: 3, offset: 0 }} className="text-3">
                             <span className="footer-highlight">&copy; </span>Freedom For You 2021
                         </Col>
                         <Col md={{ span: 5, offset: 2 }} className="text-3">
-                            Made with <i className="fas fa-heart" /> by the <i className="footer-highlight">Freedom4U Team</i>
+                            Made with <i className="fas fa-heart" /> by the <i className="footer-highlight">F4U Web Team</i>
                         </Col>
                         <Col md={{ offset: 0 }}>
-                            <a href="https://www.facebook.com/4ufreedom" target="_blank"><i className="fab fa-facebook-square mx-2" /></a>
-                            <a href="https://twitter.com/4youfreedom" target="_blank mx-2"><i className="fab fa-twitter" /></a>
-                            <a href="#" target="_blank"><i className="fab fa-youtube mx-2" /></a>
-                            <a href="mailto:4ufreedom@gmail.com" target="_blank"><i className="fas fa-envelope mx-1" /></a>
-                            <a href="tel:1234567890" target="_blank"><i className="fas fa-phone-square-alt mx-2" /></a>
+                            <a href="https://www.facebook.com/4ufreedom" target="_blank" rel="noreferrer"><i className="fab fa-facebook-square mx-1" /></a>
+                            <a href="https://twitter.com/4youfreedom" target="_blank" rel="noreferrer"><i className="fab fa-twitter mx-1" /></a>
+                            <a href="https://www.youtube.com" target="_blank" rel="noreferrer"><i className="fab fa-youtube mx-1" /></a>
+                            <a href="mailto:4ufreedom@gmail.com" target="_blank" rel="noreferrer"><i className="fas fa-envelope mx-1" /></a>
+                            <a href="tel:1234567890" target="_blank" rel="noreferrer"><i className="fas fa-phone-square-alt mx-1" /></a>
                         </Col>
                     </Row>
                 </Container>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,36 @@
+import { Container, Card, Row, Button, Jumbotron, Image } from 'react-bootstrap';
+import PreviousProjectsDesktop from '../components/projects/PreviousProjectsDesktop';
+import jumbotron from '../img/projects/jumbotron.jpg';
+
 const Projects = () => {
     return (
-        <h1 className="text-2">Hello Projects!</h1>
+        <Container fluid className="parallax">
+            <Jumbotron style={{ position: 'relative', padding: 0 }}>
+                <Image className="" src={jumbotron} alt="Jumbotron" fluid style={{ width: '100%', height: '600px' }} />
+                <div className="p-5 parallax" style={{ position: 'absolute', top: 0}}>
+                    <h1 className="text-white mb-4">We prioritize Giving!</h1>
+                    <p className="text-white">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione neque dolore laborum, sequi nihil labore perspiciatis accusantium numquam esse optio nam facilis tempora qui cum earum reiciendis, iure pariatur expedita quidem ducimus dignissimos, consectetur reprehenderit commodi. Fugit cumque iusto tempore repellendus nesciunt laboriosam itaque, quia praesentium impedit nisi esse dolore delectus et ab a accusantium perspiciatis pariatur, sint quod sequi error adipisci minima. Perspiciatis aliquid voluptatem incidunt recusandae asperiores libero natus iusto aliquam molestias minus explicabo, laborum assumenda, impedit inventore error laudantium consequatur, doloribus illum magni cum minima facere repellendus! Corporis maiores necessitatibus quo neque error omnis officia dignissimos! <br/> <br/>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste minus culpa eum fugiat pariatur ab vitae debitis ut ad aperiam reiciendis harum, error tenetur velit voluptate autem, placeat deserunt explicabo ipsa incidunt modi cum ducimus et! Maiores possimus vitae, suscipit excepturi voluptates, ut enim nobis magnam officia, facere labore praesentium magni ipsum similique nostrum placeat. Eligendi, odio! Recusandae reiciendis architecto velit. Blanditiis at sit repudiandae, quo quam iste voluptatem quidem id ex quos molestias exercitationem, fugit autem accusantium. Illum consequatur excepturi accusantium natus, incidunt placeat, rem consectetur hic delectus illo commodi omnis eligendi vitae. Sunt id dolore deserunt quod praesentium! <br /> <br />
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias voluptate qui adipisci et ea perferendis error, inventore animi vero! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat officia nemo earum impedit aliquid saepe eos, similique aliquam eveniet mollitia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, exercitationem fugit cumque saepe commodi itaque! Iure laudantium ea possimus beatae?
+                    </p>
+                    <Row>
+                        <Button variant="info" className="mx-auto">Know More</Button>
+                    </Row>
+                </div>
+            </Jumbotron>
+            <Row>
+                <Card className="p-4" border='info'>
+                    <Row className="mb-3" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <Button variant="outline-info" className="mx-2">Covid-19</Button>
+                        <Button variant="outline-info" className="mx-2">Ongoing Work</Button>
+                        <Button variant="outline-info" className="mx-2" active>Previous Projects</Button>
+                    </Row>
+                    <PreviousProjectsDesktop />
+                    <PreviousProjectsDesktop />
+                </Card>
+            </Row>
+        </Container>
     );
 }
 
